d29abc5f4f8edf4189b7f5d4a18ca464f44f589d
Dimuthchathu101 <dimuthchathu101@gmail.com>
2024-01-24 15:43:45 +0530

    Button Integration 02

--- tests/basicinteractions.spec.ts
+++ tests/basicinteractions.spec.ts
@@ -1,8 +1,8 @@
 const { test, expect, describe } = require('@playwright/test');
 import { chromium } from '@playwright/test';
 
-describe('Button Interactions ', () => {
-    test('should first', async () => { 
+describe('Interactions ', () => {
+    test('Interaction with inputs', async () => { 
         const browser = await chromium.launch({
             headless: false
         });
@@ -12,13 +12,36 @@ describe('Button Interactions ', () => {
 
         // Goto the page
         await page.goto("https://www.lambdatest.com/selenium-playground/simple-form-demo");
-
+        
         const messageInput = page.locator("input#user-message")
+        await messageInput.scrollIntoViewIfNeeded()
         await console.log(messageInput.getAttribute("placeholder"));
         expect(messageInput).toHaveAttribute("placeholder","Please Enter your Message")
 
+        console.log(await "Before Entering Data: "+messageInput.inputValue())
+        await messageInput.type("Hello Dimuth")
+        console.log(await "After Entering Data: "+messageInput.inputValue())
         // Final - Close the browser
         await browser.close();
 
+
+    });
+
+    test('SUM', async ({page}) => { 
+        await page.goto("https://www.lambdatest.com/selenium-playground/simple-form-demo");
+        const sum1input = page.locator('#sum1')
+        const sum2input = page.locator('#sum2')
+
+        const btn_get_Values = page.locator("")
+        let num1 = 124;
+        let num2 = 123;
+
+        await sum1input.type(""+num1)
+        await sum2input.type(""+num2)
+        await btn_get_Values.click()
+
+        const result = page.locator("#addmessage");
+        console.log("The result is" + result.textContent())
+        expect(result).toHaveText("Some Text Here")
     });
 });
